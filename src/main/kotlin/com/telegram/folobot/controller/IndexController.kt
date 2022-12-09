44ee31ab9270d62b4872c04704eb6d94a851731a
everbald <54261474+everbald@users.noreply.github.com>
2022-12-09 23:08:43 +0300

    [MINOR] resource fix

--- src/main/kotlin/com/telegram/folobot/controller/IndexController.kt
+++ src/main/kotlin/com/telegram/folobot/controller/IndexController.kt
@@ -1,16 +1,22 @@
 package com.telegram.folobot.controller
 
 import mu.KLogging
-import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.*
-import java.io.File
+import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class IndexController() : KLogging() {
-    @GetMapping(value=["", "/", "index"])
+    @GetMapping(value = ["", "/", "index"])
     fun index(model: MutableMap<String, Any>): String {
-        model["rndvid"] = "media/${ File("src/main/resources/static/media").list()?.random() }"
+        model["rndvid"] = "media/${
+            listOf(
+                "dancefo.mp4",
+                "dancefodance.mp4",
+                "dothefo.mp4",
+                "eatfoeat.mp4",
+                "runforun.mp4",
+            ).random()
+        }"
         return "index"
 //        return "redirect:/frontpage";
     }
