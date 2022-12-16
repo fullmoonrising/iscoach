6331dabb551fb354c282af8bdaf9face084dce8f
everbald <54261474+everbald@users.noreply.github.com>
2022-12-16 21:54:06 +0300

    [MINOR] chore work

--- postgres/backup.sh
+++ postgres/backup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-comand="pg_dump --username=$DB_USER --dbname=$DB_NAME --file=/var/lib/postgresql/data/backup/backup-`date +%Y-%m-%d-%H-%M-%S`.sql --column-inserts --table=folo*"
-echo $comand
+find ./data/backup/ -mtime +3 -delete
 docker exec -i db bash -c "pg_dump --username=$DB_USER --dbname=$DB_NAME --file=/var/lib/postgresql/data/backup/backup-`date +%Y-%m-%d-%H-%M-%S`.sql --column-inserts --table=folo*"
